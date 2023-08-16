a3259d8aa627e2f1c8339f7e7b4896c91837a397
Kyle Conroy <kyleconroy@gmail.com>
2023-08-16 13:22:29 -0400

    support loading region from file in footprints plugin
    * currently without any repositioning support (region is static)

--- jdaviz/core/template_mixin.py
+++ jdaviz/core/template_mixin.py
@@ -768,6 +768,8 @@ class FileImportSelectPluginComponent(SelectPluginComponent):
     def __init__(self, plugin, **kwargs):
         """
         """
+        self._cached_file = {}
+
         if "From File..." not in kwargs['manual_options']:
             kwargs['manual_options'] += ['From File...']
 
