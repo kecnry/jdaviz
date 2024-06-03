e07b188f4479fd647bf3e21bba20486bffdfec51
Kyle Conroy <kyleconroy@gmail.com>
2024-06-03 08:45:48 -0400

    duplicate line analysis plugin

--- jdaviz/configs/specviz/plugins/__init__.py
+++ jdaviz/configs/specviz/plugins/__init__.py
@@ -2,3 +2,4 @@ from .viewers import *  # noqa
 from .parsers import *  # noqa
 from .unit_conversion.unit_conversion import *  # noqa
 from .line_analysis.line_analysis import *  # noqa
+from .line_analysis_solara.line_analysis_solara import *  # noqa
