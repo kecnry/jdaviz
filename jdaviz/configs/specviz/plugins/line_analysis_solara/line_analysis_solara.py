d8ed534b95c04dc4b466c26614f0b57e2f09bbcf
Kyle Conroy <kyleconroy@gmail.com>
2024-06-04 06:12:21 -0400

    separate state from user-API

--- jdaviz/configs/specviz/plugins/line_analysis_solara/line_analysis_solara.py
+++ jdaviz/configs/specviz/plugins/line_analysis_solara/line_analysis_solara.py
@@ -3,54 +3,58 @@ from jdaviz.core.registries import tray_registry
 from jdaviz.core.user_api import PluginUserApi
 
 
-__all__ = ['LineAnalysisSolara', 'LineAnalysisSolaraPlugin']
+__all__ = ['LineAnalysisSolara']
+
+# separate state
+class PluginState:
+    def __init__(self, *args, **kwargs):
+        self.sentence = solara.reactive("Solara makes our team more productive")
+        self.word_limit = solara.reactive(10)  
 
 
 @solara.component
-def LineAnalysisSolara(sentence, word_limit, do_something):
+def PluginUI(state, plugin):
     # Calculate word_count within the component to ensure re-execution when reactive variables change.
-    word_count = len(sentence.value.split())
+    word_count = len(state.sentence.value.split())
 
-    solara.SliderInt("Word limit", value=word_limit, min=2, max=20)
-    solara.InputText(label="Your sentence", value=sentence, continuous_update=True)
+    solara.SliderInt("Word limit", value=state.word_limit, min=2, max=20)
+    solara.InputText(label="Your sentence", value=state.sentence, continuous_update=True)
 
-    solara.Button("do something", on_click=do_something)
+    solara.Button("do something", on_click=plugin.increment_word_limit)
 
     # Display messages based on the current word count and word limit.
-    if word_count >= int(word_limit.value):
-        solara.Error(f"With {word_count} words, you passed the word limit of {word_limit.value}.")
-    elif word_count >= int(0.8 * word_limit.value):
-        solara.Warning(f"With {word_count} words, you are close to the word limit of {word_limit.value}.")
+    if word_count >= int(state.word_limit.value):
+        solara.Error(f"With {word_count} words, you passed the word limit of {state.word_limit.value}.")
+    elif word_count >= int(0.8 * state.word_limit.value):
+        solara.Warning(f"With {word_count} words, you are close to the word limit of {state.word_limit.value}.")
     else:
         solara.Success("Great short writing!")
 
 
 @tray_registry('solara-line-analysis', label="Line Analysis (Solara)", viewer_requirements='spectrum')
-class LineAnalysisSolaraPlugin():
+class LineAnalysisSolara():
 
     def __init__(self, app=None, tray_instance=False):
-        self._sentence = solara.reactive("Solara makes our team more productive")
-        self._word_limit = solara.reactive(10)  
-
-        self._widget = LineAnalysisSolara.widget(sentence=self._sentence, word_limit=self._word_limit,
-                                                 do_something=self.do_something)
+        self._state = PluginState()
+        self._widget = PluginUI.widget(state=self._state, plugin=self)
 
     @property
     def user_api(self):
-        return PluginUserApi(self, expose=('sentence', 'word_limit', 'do_something'),
+        return PluginUserApi(self, expose=('sentence', 'word_limit', 'increment_word_limit'),
                              readonly=('word_limit',))
 
     @property
     def word_limit(self):
-        return self._word_limit.value
+        return self._state.word_limit.value
 
     @property
     def sentence(self):
-        return self._sentence.value
+        return self._state.sentence.value
 
     @sentence.setter
     def sentence(self, value):
-        self._sentence.value = value
+        self._state.sentence.value = value
 
-    def do_something(self, *args):
-        self._word_limit.value += 1
+    def increment_word_limit(self, *args):
+        self._state.word_limit.value += 1
+        return self._state.word_limit.value
