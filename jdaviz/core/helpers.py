06eb978d38976f97089071d535bdcd021bf83748
Kyle Conroy <kyleconroy@gmail.com>
2025-09-02 14:42:21 -0400

    deprecate load_data, remove old parsers

--- jdaviz/core/helpers.py
+++ jdaviz/core/helpers.py
@@ -448,7 +448,7 @@ class ConfigHelper(HubListener):
             self.app.layout.height = height
             self.app.state.settings['context']['notebook']['max_height'] = height
 
-        if self.app.config in ('specviz', 'specviz2d', 'lcviz') or self.app.state.dev_loaders:
+        if self.app.config in ('specviz', 'specviz2d', 'imviz', 'cubeviz', 'lcviz') or self.app.state.dev_loaders:  # noqa
             if not len(self.viewers) and not len(self.app.state.drawer_content):
                 self.app.state.drawer_content = 'loaders'
             else:
